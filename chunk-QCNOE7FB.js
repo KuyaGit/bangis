7d6652b6a6165c01fc068ac7857f0b5a07596221
KuyaGit <KuyaGit@users.noreply.github.com>
2024-02-22 17:18:23 +0000

    deploy: 85033cdeaf050d3d5138e42de3a177cc142a2808

---
+++ chunk-QCNOE7FB.js
@@ -0,0 +1,1 @@
+var e={production:!0,baseUrl:"https://bangis.azurewebsites.net",tokenName:"bangis_authToken123",theme:"themeColor"};export{e as a};
--- chunk-UCEYOKWH.js
+++
@@ -1,1 +0,0 @@
-var e={production:!0,baseUrl:"https://bangis.azurewebsites.net",tokenName:"bangis_authToken123"};export{e as a};
