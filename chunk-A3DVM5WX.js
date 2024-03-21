6e4ef351f0cd28c19cbc969493390b0ce2e412ce
KuyaGit <KuyaGit@users.noreply.github.com>
2024-03-21 04:30:47 +0000

    deploy: b0208f8c6eda18e72394b0b3a401daa331780994

---
+++ chunk-A3DVM5WX.js
@@ -0,0 +1,1 @@
+var e={production:!0,baseUrl:"https://bangis-api.onrender.com",tokenName:"bangis_authToken123",theme:"themeColor"};export{e as a};
--- chunk-QCNOE7FB.js
+++
@@ -1,1 +0,0 @@
-var e={production:!0,baseUrl:"https://bangis.azurewebsites.net",tokenName:"bangis_authToken123",theme:"themeColor"};export{e as a};
