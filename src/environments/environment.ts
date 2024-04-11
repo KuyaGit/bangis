9f398131af7206c5229bda6c12ef7316b8cc5938
Mark Lowel Montealto <102800815+KuyaGit@users.noreply.github.com>
2024-04-11 16:02:20 +0800

    fix api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://bangis-de62b707cdd6.herokuapp.com/',
+  baseUrl: 'https://bangis-de62b707cdd6.herokuapp.com',
   tokenName: 'bangis_authToken123',
   theme: 'themeColor',
 };
