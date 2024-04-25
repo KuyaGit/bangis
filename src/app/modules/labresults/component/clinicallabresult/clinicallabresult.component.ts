2c879b52ebc1a260d9f33aac343f26b12d8585cf
KuyaGit <marklowelmontealto14@gmail.com>
2024-04-25 19:03:47 +0800

    lab-result-fix

--- src/app/modules/labresults/component/clinicallabresult/clinicallabresult.component.ts
+++ src/app/modules/labresults/component/clinicallabresult/clinicallabresult.component.ts
@@ -72,7 +72,7 @@ export class ClinicallabresultComponent{
   isRequired: boolean = false;
   addLabResult() {
     this.isLoadingButton.set(true);
-    if(this.labForm.invalid){
+    if(!this.labForm.get('specimen')?.value || !this.labForm.get('testMethod')?.value || !this.labForm.get('fat')?.value || !this.labForm.get('recommendation')?.value || !this.labForm.get('diagnosis')?.value || !this.labForm.get('remarks')?.value || !this.labForm.get('labAccession')?.value || !this.labForm.get('owner')?.value || !this.labForm.get('address')?.value || !this.labForm.get('sender')?.value || !this.labForm.get('senderAddress')?.value || !this.labForm.get('email')?.value || !this.labForm.get('contactNumber')?.value || !this.labForm.get('')){
       this._alertS.handleError('Please fill in all required fields');
       this.isRequired = true;
       this.isLoadingButton.set(false);
@@ -90,7 +90,7 @@ export class ClinicallabresultComponent{
         this.isLoadingButton.set(false);
         this.modalEvent.emit(false);
         this._alertS.handleSuccess('Lab result added successfully');
-
+        this.getAllMethod.emit(this.subscription);
       }
     });
   }
