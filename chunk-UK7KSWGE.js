35bbea88596d3170fab119f04d98fba0f2c3803b
KuyaGit <102800815+KuyaGit@users.noreply.github.com>
2024-07-03 02:54:18 +0000

    Build: (68b8752) New API Link

--- chunk-UK7KSWGE.js
+++
@@ -1,1 +0,0 @@
-var e={production:!0,baseUrl:"https://bangis-de62b707cdd6.herokuapp.com",tokenName:"bangis_authToken123",theme:"themeColor"};export{e as a};
---
+++ chunk-XWYQU7EN.js
@@ -0,0 +1,1 @@
+var e={production:!0,baseUrl:"https://web-production-2ceb.up.railway.app",tokenName:"bangis_authToken123",theme:"themeColor"};export{e as a};
